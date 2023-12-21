2ba0f5eb0b799031aec0dbed2d06df8040661f83
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2023-12-21 21:56:41 +0900

    SwiftUIで実装するViewのファイルを追加

---
+++ VIPER-SwiftUI-Combine-Sample/VIPER-SwiftUI-Combine-Sample/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  VIPER-SwiftUI-Combine-Sample
+//
+//  Created by home on 2023/12/21.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstView()
+}
