a3a0fc7eccd31b38fadcaa0d7dfec63ef36bc2de
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2023-12-23 23:24:20 +0900

    FirstView用のRouterを追加

---
+++ VIPER-SwiftUI-Combine-Sample/VIPER-SwiftUI-Combine-Sample/FirstViewRouter.swift
@@ -0,0 +1,14 @@
+//
+//  FirstViewRouter.swift
+//  VIPER-SwiftUI-Combine-Sample
+//
+//  Created by home on 2023/12/23.
+//
+
+import Foundation
+
+struct FirstViewRouter {
+    func presentDetail() {
+        print("画面遷移した")
+    }
+}
