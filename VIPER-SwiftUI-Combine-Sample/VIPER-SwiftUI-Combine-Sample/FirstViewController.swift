320a3cd64765270fd7430e0cc158eaeca415f004
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2023-12-26 22:53:50 +0900

    UIViewControllerでSwiftUIのViewを使う

--- VIPER-SwiftUI-Combine-Sample/VIPER-SwiftUI-Combine-Sample/FirstViewController.swift
+++ VIPER-SwiftUI-Combine-Sample/VIPER-SwiftUI-Combine-Sample/FirstViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import SwiftUI
 
 class FirstViewController: UIViewController {
 
+    private let firstViewState = FirstView.State(cartProducts: [])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let rootView = FirstView(delegate: self, state: firstViewState)
+        let hostingVC = UIHostingController(rootView: rootView)
+        addChild(hostingVC)
+        hostingVC.didMove(toParent: self)
+        view.addSubview(hostingVC.view)
     }
 
 
